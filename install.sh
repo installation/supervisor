0c1cf9772ae3476326bc5bcab7f80cfd174026c7
Sági-Kazár Márk <webmaster@firstcomputer.hu>
2013-08-27 11:56:06 +0200

    Supervisord install developped

--- install.sh
+++ install.sh
@@ -6,82 +6,132 @@
 #
 # Version: 3.0
 
-# Function definitions
+# Basic function definitions
 
 ## Echo colored text
 e()
 {
-	color="\033[${2:-34}m"
+	local color="\033[${2:-34}m"
 	echo -e "$color$1\033[0m"
 }
+
+## Exit error
+ee()
+{
+	local exit_code="${2:-1}"
+	local color="${3:-31}"
+
+	e "$1" "$color"
+	exit $exit_code
+}
+
+# Checking root access
+if [ $EUID -ne 0 ]; then
+	ee "This script has to be ran as root!"
+fi
+
+# Variable definitions
+DIR=$(cd `dirname $0` && pwd)
+NAME="Supervisord"
+VER="3.0"
+DEPENDENCIES=("python" "dialog" "tar")
+TMP="/tmp/$NAME"
+INSTALL_LOG="$TMP/install.log"
+ERROR_LOG="$TMP/error.log"
+
+# CTRL_C trap
+ctrl_c()
+{
+	clear
+	echo
+	echo "Installation aborted by user!"
+	rm -rf $TMP/supervisor* $TMP/setuptools*
+}
+trap ctrl_c INT
+
+# Basic checks
+
+## Check for wget or curl or fetch
+e "Checking for HTTP client..."
+if [ `which curl &> /dev/null` ]; then
+	download="$(which curl) -O"
+elif [ `which wget &> /dev/null` ]; then
+	download="$(which wget) --no-certificate"
+elif [ `which fetch &> /dev/null` ]; then
+	download="$(which fetch)"
+else
+	DEPENDENCIES+=("wget")
+	download="$(which wget) --no-certificate"
+	e "No HTTP client found, wget added to dependencies" 31
+fi
+
+## Check for package manager (apt or yum)
+e "Checking for package manager..."
+if [ `which apt-get &> /dev/null` ]; then
+	install="$(which apt-get) -y --force-yes install"
+elif [ `which yum &> /dev/null` ]; then
+	install="$(which yum) -y install"
+else
+	ee "No package manager found."
+fi
+
+## Check for init system (update-rc.d or chkconfig)
+e "Checking for init system..."
+if [ `which update-rc.d &> /dev/null` ]; then
+	init="$(which update-rc.d)"
+elif [ `which chkconfig &> /dev/null` ]; then
+	init="$(which chkconfig) --add"
+else
+	ee "Init system not found, service not started!"
+fi
+
+## Clearing logs
+rm -rf $INSTALL_LOG $ERROR_LOG
+
+# Function definitions
 
 ## Install required packages
 install()
 {
 	if [ -z "$1" ]; then
+		e "Package not given" 31
 		return 1
 	else
-		if [ `which apt-get 2> /dev/null` ]; then
-			apt-get install -y -qq $1 &> /dev/null || return 2
-		elif [ `which yum 2> /dev/null` ]; then
-			yum install -y -q $1 &> /dev/null || return 2
-		else
-			return 3
-		fi
-		return 0
+		e "Installing package: $1"
+		$install "$1" >> $INSTALL_LOG 2>> $ERROR_LOG || ee "Error during package install"
+		e "Package $1 successfully installed"
 	fi
+
+	return 0
+}
+
+download()
+{
+	if [ -z "$1" ]; then
+		e "No download given" 31
+		return 1
+	else
+		$download "$1" >> $INSTALL_LOG 2>> $ERROR_LOG || ee "Error during download"
+	fi
+
+	return 0
 }
 
 ## Show progressbar
 progress()
 {
-	progress=${1:-0}
-	gauge="${2:-Please wait}"
-	title="${3:-Installation progress}"
+	local progress=${1:-0}
+	local gauge="${2:-Please wait}"
+	local title="${3:-Installation progress}"
 
 	echo $progress | dialog --backtitle "Installing $NAME $VER" \
 	 --title "$title" --gauge "$gauge" 7 70 0
 }
 
-# Variable definitions
-
-DIR=$(cd `dirname $0` && pwd)
-NAME="Supervisord"
-VER="3.0"
-DEPENDENCIES=("python" "dialog" "wget" "tar")
-
-# Checking root access
-if [ $EUID -ne 0 ]; then
-	e "This script has to be ran as root!" 31
-	exit 1
-fi
-
 # Checking dependencies
 for dep in ${DEPENDENCIES[@]}; do
-	if [ ! $(which $dep 2> /dev/null) ]; then
-		e "Installing package: $dep"
+	if [ ! $(which $dep &> /dev/null) ]; then
 		install "$dep"
-		case $? in
-			0 )
-				e "Package installed: $dep"
-				;;
-			1 )
-				e "Invalid package: $dep" 31
-				exit 1
-				;;
-			2 )
-				e "Package install failed: $dep" 31
-				exit 1
-				;;
-			3 )
-				e "Package manager not found" 31
-				exit 1
-				;;
-			* )
-				e "Undefined Error" 31
-				exit 1
-				;;
-		esac
 	fi
 done
 
@@ -95,8 +145,7 @@ if [ -f /usr/local/bin/supervisord ]; then
 			e "Installing $NAME over the previous version" 31
 			;;
 		* )
-			e "Installation aborted"
-			clear
+			ctrl_c
 			exit 0
 			;;
 	esac
@@ -109,33 +158,39 @@ config=$(dialog --stdout --backtitle "Installing $NAME $VER" \
  2 "Predefined config" on \
  3 "Open editor" off)
 
-cd /tmp
+mkdir -p $TMP
+cd $TMP
 
 progress 15 "Cleaning up"
-rm -rf supervisor* setuptools*
+rm -rf *
 
 progress 30 "Downloading files"
-wget --quiet --no-check-certificate https://pypi.python.org/packages/source/s/supervisor/supervisor-3.0.tar.gz > /dev/null
-wget --quiet --no-check-certificate https://pypi.python.org/packages/source/s/setuptools/setuptools-1.0.tar.gz > /dev/null
+download https://pypi.python.org/packages/source/s/supervisor/supervisor-3.0.tar.gz
+download https://pypi.python.org/packages/source/s/setuptools/setuptools-1.0.tar.gz
 
 progress 45 "Extracting files"
-tar -xvzf supervisor-3.0.tar.gz > /dev/null
-tar -xvzf setuptools-1.0.tar.gz > /dev/null
+tar -xvzf supervisor-3.0.tar.gz >> $INSTALL_LOG 2>> $ERROR_LOG
+tar -xvzf setuptools-1.0.tar.gz >> $INSTALL_LOG 2>> $ERROR_LOG
 
 progress 60 "Installing Setuptools"
 cd setuptools-1.0
-python setup.py install > /dev/null
+python setup.py install >> $INSTALL_LOG 2>> $ERROR_LOG
 
 progress 75 "Installing $NAME $VER"
 cd ../supervisor-3.0
-python setup.py install > /dev/null
+python setup.py install >> $INSTALL_LOG 2>> $ERROR_LOG
 
-cd ..
+cd $TMP
 
 progress 90 "Setting up $NAME $VER"
 case $config in
 	2 )
-		cp $DIR/config/supervisord.conf /etc/
+		if [ -f $DIR/config/supervisord.conf ]; then
+			cp -r $DIR/config/supervisord.conf /etc/
+		else
+			download http://github.com
+			mv supervisord.conf /etc/
+		fi
 		;;
 	3 )
 		echo_supervisord_conf >> /etc/supervisord.conf
@@ -157,18 +212,13 @@ rm -rf setuptools* supervisor*
 
 clear
 
-#curl https://raw.github.com/gist/176149/88d0d68c4af22a7474ad1d011659ea2d27e35b8d/supervisord.sh > /etc/init.d/supervisord
-cp $DIR/supervisord /etc/init.d/supervisord
-chmod +x /etc/init.d/supervisord
-
-if [ `which update-rc.d 2> /dev/null` ]; then
-	update-rc.d supervisord defaults &> /dev/null || e "Error installing init script!" 31
-elif [ `which chkconfig 2> /dev/null` ]; then
-	chkconfig --add supervisord &> /dev/null || e "Error installing init script!" 31
+if [ -f $DIR/supervisord ]; then
+	cp -r $DIR/supervisord /etc/init.d/supervisord
 else
-	e "InitV not found, service not started!" 31
-	exit 1
+	download http://github.com
+	mv supervisord /etc/init.d/supervisord
 fi
+chmod +x /etc/init.d/supervisord
 
 service supervisord stop
 service supervisord start
